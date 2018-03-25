ac7465c66d1b7b07155d4d24305430e0381cc185
Stefanos Mitropoulos <stefmitropoulos@gmail.com>
2018-03-25 23:57:35 +0300

    Initial Commit

---
+++ Strings are your friends, until they betray you./main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Strings are your friends, until they betray you.
+//
+//  Created by Stefanos Mitropoulos on 25/03/2018.
+//  Copyright © 2018 Stefanos Mitropoulos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
