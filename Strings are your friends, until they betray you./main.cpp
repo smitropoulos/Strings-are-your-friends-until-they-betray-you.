c38736a0b5db1bb1b28e28593513af3af76efb05
Stefanos Mitropoulos <stefmitropoulos@gmail.com>
2018-03-26 00:38:09 +0300

    init

--- Strings are your friends, until they betray you./main.cpp
+++ Strings are your friends, until they betray you./main.cpp
@@ -9,7 +9,27 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+	std::string fname,lname;
+
+	std::cout<<"Enter your first name please"<<std::endl;
+	std::cin >>fname;
+	std::cout<<"Enter your last name please"<<std::endl;
+	std::cin >>lname;
+
+	std::string fullname = fname+" "+lname;
+	std::string fullname2=fullname;
+
+	for (int i=0;i<fullname.size();i++){
+		if (fullname[i]=='a'){
+			fullname[i]='z';
+		}
+	}
+
+	std::cout<<"Your name is: "<<fullname<<std::endl;
+
+	//or in reverse
+	std::reverse(fullname2.begin(), fullname2.end());
+	std::cout<<"Your name in reverse is: "<<fullname2<<std::endl;
+
 	return 0;
 }
